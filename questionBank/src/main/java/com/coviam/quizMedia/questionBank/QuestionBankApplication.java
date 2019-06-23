4d614dabe56737b0df3fb6d3fa7024cf3d7dbf38
MAYUR MISHRA <mayurmishra14@gmail.com>
2019-06-23 16:11:24 +0530

    SQ-004: adding db name in question bank

--- questionBank/src/main/java/com/coviam/quizMedia/questionBank/QuestionBankApplication.java
+++ questionBank/src/main/java/com/coviam/quizMedia/questionBank/QuestionBankApplication.java
@@ -1,13 +1,25 @@
 package com.coviam.quizMedia.questionBank;
 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 @SpringBootApplication
-public class QuestionBankApplication {
+public class QuestionBankApplication extends AbstractMongoConfiguration {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionBankApplication.class, args);
 	}
 
+	@Override
+	protected String getDatabaseName() {
+		return "questionBankDb";
+	}
+
+	@Override
+	public Mongo mongo() throws Exception {
+		return new MongoClient("localhost",27017);
+	}
 }
